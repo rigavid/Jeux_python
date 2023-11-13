335c42aec8c33188182967466ca898d178cf00fb
T-Sana <110158900+T-Sana@users.noreply.github.com>
2023-11-13 20:05:01 +0100

    Q

--- Outils/couleurs.py
+++ Outils/couleurs.py
@@ -7,14 +7,18 @@ class col:
     cyan=[0,255,255]
     magenta=[255,0,255]
     jaune=yellow=[255,255,0]
-    def new(hexadecimal='000000',type='bgr'):
+    def new(hexadecimal='000000',tipe='bgr'):
         '''
             Couleur héxadécimale en BGR par défaut.
             Couleur héxadécimale en RGB si c'est spécifié sur le type.
             ---
             Retourne une couleur en BGR
         '''
+        if type(hexadecimal) == int:
+            hexadecimal = f'{hexadecimal:x}'
         hexadecimal.replace('#','')
-        if type.lower()=='rgb':r,g,b=int(hexadecimal[0:2],base=16),int(hexadecimal[2:4],base=16),int(hexadecimal[4:6],base=16)
-        else:b,g,r=int(hexadecimal[0:2],base=16),int(hexadecimal[2:4],base=16),int(hexadecimal[4:6],base=16)
+        if tipe.lower()=='rgb':
+            r, g, b = int(hexadecimal[0:2],base=16), int(hexadecimal[2:4],base=16), int(hexadecimal[4:6],base=16)
+        else:
+            b, g, r = int(hexadecimal[0:2],base=16), int(hexadecimal[2:4],base=16), int(hexadecimal[4:6],base=16)
         return[b,g,r]
