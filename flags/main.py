cc91cb2133bcc1fad6c7deb8156e34465606d905
T-Sana <110158900+T-Sana@users.noreply.github.com>
2024-12-10 19:58:44 +0100

    French alpha version of flag finding game complete. Multilingual support added

--- flags/main.py
+++ flags/main.py
@@ -3,10 +3,10 @@ from PIL import Image
 from cairosvg import svg2png
 from io import BytesIO
 
-def openSVG(path):
-    img = new_img()
+def openflag(path):
+    img = new_img(name=path.split("/")[-1])
     with open(path, "r") as file:
-        img.img = cv2.cvtColor(np.array(Image.open(BytesIO(svg2png(bytestring=file.read()))).convert('RGBA')), cv2.COLOR_RGBA2BGRA)
+        img.img = cv2.cvtColor(np.array(Image.open(BytesIO(svg2png(bytestring=file.read(), output_height=RES.resolution[1]*60//100))).convert('RGB')), cv2.COLOR_RGB2BGR)
     return img
 
 COL.DarkModeBG = COL.new("#1e1e1e")
@@ -16,80 +16,97 @@ class game:
     for i in os.listdir("/".join(i for i in __file__.split("/")[:-1:])+f"/langues/"):
         with open("/".join(i for i in __file__.split("/")[:-1:])+f"/langues/{i}", "r", encoding="utf8") as file:
             _infos_ += f"'{i[:-4:]}':{file.read()},\n"
-    flags = [f"{"/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux/"}/{i}" for i in os.listdir("/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux/")]
+    flags = [f"{"/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux"}/{i}" for i in os.listdir("/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux/")]
     _infos_ = eval((_infos_ + "}").replace("\n", ""))
     def reload_infos(self) -> None:
         self.infos = self._infos_[self.lang]
         self.name = self.infos["title"]
         self.buttons = self.infos["buttons"]
         self.flags_names = self.infos["flags"]
-    def load_games(self) -> None:
-        try:
-            with open("/".join(i for i in __file__.split("/")[:-1:])+"/games.txt", "r", encoding="utf8") as file:
-                self.games = eval(file.read())
-        except: self.games = []
     def __init__(self, lang="fr"):
         self.lang = lang if lang in self._infos_.keys() else "fr"
         self.reload_infos()
-        self.load_games()
         self.mode = True
         self.img = new_img(name=self.name)
         self.round = 0
-    def image(self) -> None:
+        self.random = False
+    def image(self, UI=True) -> None:
         img = new_img(RES.resolution, COL.DarkModeBG if self.mode else COL.lightGray)
-        coosRound = RES.percentile(80, 10), RES.percentile(90, 15)
-        img.rectangle(*coosRound, COL.red, 0)
-        img.write_centered(f"Round: {self.round+1}", ct_sg(*coosRound), COL.lime, 1, 1, FONTS[FONT_NAMES[0]])
+        if UI: ## Draws UI
+            coosRound = RES.percentile(80, 10), RES.percentile(90, 15)
+            img.rectangle(*coosRound, COL.red, 0)
+            img.write_centered(f"Round: {self.round+1}", ct_sg(*coosRound), COL.lime, 1, 1, FONTS[FONT_NAMES[0]])
         self.img.img = img.img
-    def guess_img(self, flag, guess):
+    def guess_img(self, guess):
         self.image()
-        drp = openSVG(flag)
-        pt_d = [(self.img.size()[0]-self.drp.size()[0])/2, (self.img.size()[1]-self.drp.size()[1])/2]
-        pt_f = [pt_d[0]+self.drp.size()[0], pt_d[1]+self.drp.size()[1]]
-        self.img.img[pt_d] ## TODO
+        pt_d = [(self.img.size()[0]-self.flag.size()[0])//2, (self.img.size()[1]-self.flag.size()[1])//2]
+        pt_f = [pt_d[0]+self.flag.size()[0], pt_d[1]+self.flag.size()[1]]
+        self.img.img[pt_d[1]:pt_f[1], pt_d[0]:pt_f[0]] = self.flag.img
         self.img.write_centered(guess, RES.percentile(50, 80), COL.lime, 5, 5, FONTS[FONT_NAMES[0]])
-
+        if self.show_an:
+            self.img.write_centered(self.show_an, RES.percentile(50, 15), COL.red, 4, 4, FONTS[FONT_NAMES[0]])
     def guess_flag(self, flag):
         guess = ""
-        self.flag = openSVG()
+        self.flag = openflag(flag)
+        self.show_an = None
+        self.guess_img(guess)
+        try: country_names = [i.replace(" ", "").replace("-", "").replace("\n", "").lower() for i in eval(self.infos["flags"][self.flag.name.split(".")[0]])]
+        except KeyError: return print(f"{self.flag.name.split(".")[0]} has no names defined yet!")
         while self.img.is_opened():
             wk = self.img.show()
             match wk:
                 case 65473 | 269025139: #f4
                     self.mode = not self.mode
-                    self.image()
+                    self.guess_img(guess)
+                case 65474: #f5
+                    self.show_an = None if self.show_an else eval(self.infos["flags"][self.flag.name.split(".")[0]])[0]
+                    self.guess_img(guess)
                 case a if a in [ord(i) for i in "abcdefghijklmnopqrstuvwxyz"]:
                     guess += chr(wk)
-                    self.guess_img(flag, guess)
+                    self.guess_img(guess)
+                    if guess in country_names:
+                        self.round += 1
+                        return
                 case 65535: ## Suppr
                     RES.update()
-                    self.guess_img(flag, guess)
+                    self.guess_img(guess)
                 case 8:
                     guess = guess[:-1:]
-                    self.guess_img(flag, guess)
+                    self.guess_img(guess)
+                case 27: return self.img.close()
+    def titleImg(self):
+        self.image(False)
+        self.img.write_centered("Titlescreen", RES.percentile(50, 30), COL.red, 10, 10, FONTS[FONT_NAMES[1]])
+        self.img.write_centered("Press enter to start", RES.percentile(50, 50), COL.red, 5, 5, FONTS[FONT_NAMES[1]])
+        self.img.write_centered(f"Mode: {"aleatoire" if self.random else "ordre"}\n'c' pour changer de mode", RES.percentile(50, 80), COL.red, 3, 3, FONTS[FONT_NAMES[1]])
+    def endImg(self):
+        self.image(False)
+        self.img.write_centered("The end", RES.percentile(50, 50), COL.red, 10, 10, FONTS[FONT_NAMES[1]])
+        self.img.write_centered("Press enter to restart", RES.percentile(50, 70), COL.red, 5, 5, FONTS[FONT_NAMES[1]])
     def titlescreen(self):
-        self.img.write_centered("Titlescreen", RES.percentile(50, 50), COL.red, 10, 10, FONTS[FONT_NAMES[1]])
-        self.img.write_centered("Press enter to start", RES.percentile(50, 70), COL.red, 5, 5, FONTS[FONT_NAMES[1]])
+        self.titleImg()
         while self.img.is_opened():
             wk = self.img.show_(0, built_in_functs=False)
             match wk:
                 case 65473 | 269025139: #f4
                     self.mode = not self.mode
-                    self.image()
+                    self.titleImg()
+                case 99:
+                    self.random = not self.random
+                    self.titleImg()
                 case 32: self.img.fullscreen = not self.img.fullscreen
                 case 13: return
                 case 27: self.img.close()
                 case -1: ...
                 case _: print(f"{wk=}")
     def endScreen(self):
-        self.img.write_centered("The end", RES.percentile(50, 50), COL.red, 10, 10, FONTS[FONT_NAMES[1]])
-        self.img.write_centered("Press enter to restart", RES.percentile(50, 70), COL.red, 5, 5, FONTS[FONT_NAMES[1]])
+        self.endImg()
         while self.img.is_opened():
             wk = self.img.show_(0, built_in_functs=False)
             match wk:
                 case 65473 | 269025139: #f4
                     self.mode = not self.mode
-                    self.image()
+                    self.endImg()
                 case 32: self.img.fullscreen = not self.img.fullscreen
                 case 13: return
                 case 27: self.img.close()
@@ -99,13 +116,18 @@ class game:
             self.img.img = new_img(RES.resolution, COL.DarkModeBG if self.mode else COL.LightModeBG).img
             self.titlescreen()
             self.image()
-            for flag in self.flags:
-                self.guess_flag(flag)
+            if self.random:
+                np.random.shuffle(game.flags)
+            for flag in game.flags:
+                if flag.split("/")[-1].split(".")[0] in self.flags_names.keys():
+                    self.guess_flag(flag)
+                    if img.is_closed(): break
             self.img.img = new_img(RES.resolution, COL.DarkModeBG if self.mode else COL.LightModeBG).img
             self.endScreen()
 
 def main():
     jeu = game()
+    jeu.img.fullscreen = True
     jeu.start()
 
 if __name__ == "__main__":
--- flags/nompays.py
+++ flags/nompays.py
@@ -1,27 +1,21 @@
 import os, pycountry as pc
-with open("/".join(i for i in __file__.split("/")[:-1:])+"/langues/fr.txt", "r", encoding="utf8") as file:
-    infos = eval(file.read().replace("\n", ""))
 
-lst = os.listdir("/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux/")
-lst.sort()
-for d in lst:
+langue = "en"
+
+try:
+    with open("/".join(i for i in __file__.split("/")[:-1:])+f"/langues/{langue}.txt", "r", encoding="utf8") as file: infos = eval(file.read().replace("\n", ""))
+except: infos = {"title":"Flags of the world","flags":{},"buttons":[]}
+
+for d in os.listdir("/".join(i for i in __file__.split("/")[:-1:])+"/drapeaux/"):
     if not (key:=d[:-4:]) in [i for i in infos["flags"]]:
         try:
-            try:
-                nom = pc.countries.get(alpha_2=key).name
-            except:
-                print(f"Le pays '{key}' n'est pas reconnu ")
-                continue
-            val = "".join(i for i in input(f"{nom}: ") if i.lower() in ",abcdefghijklmnopqrstuvwxyz-")
-            if val != "-":
-                infos["flags"][key] = set(list(val.replace("-","").split(",")))
+            try: nom = pc.countries.get(alpha_2=key).name
+            except: print(f"Le pays '{key}' n'est pas reconnu "); continue
+            if (val:="".join(i for i in input(f"{nom}: ") if i.lower() in " ,abcdefghijklmnopqrstuvwxyz-")) != "-": infos["flags"][key] = val.replace("-","").split(",")
         except KeyboardInterrupt: break
-        except Exception as e:
-            print(e)
-
+        except Exception as e: print(e)
+ind = list(infos["flags"].keys()); ind.sort()
+infos["flags"] = {k:infos["flags"][k] for k in ind}
 print(" "*100, end="\r")
-
 s = f"{"{"}\n\t'title': '{infos["title"]}',\n\t'flags': {"{"}\n\t\t{",\n\t\t".join(f"'{k}': \"{infos["flags"][k]}\"" for k in infos["flags"])}\n\t{"}"},\n\t'buttons': [\n\t\t{",\n\t\t".join(f"'{v}'" for v in infos["buttons"])}\n\t]\n{"}"}"
-
-with open("/".join(i for i in __file__.split("/")[:-1:])+"/langues/fr.txt", "w", encoding="utf8") as file:
-    file.write(s)
+with open("/".join(i for i in __file__.split("/")[:-1:])+f"/langues/{langue}.txt", "w", encoding="utf8") as file: file.write(s)
